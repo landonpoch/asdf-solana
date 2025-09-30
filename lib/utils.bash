69f23d0715ba73676071ea1526e1b65dc5ac0cb2
Landon Poch <landon.poch+git@gmail.com>
2025-09-30 17:25:33 -0600

    Not deleting install artifacts for debugging purposes.

--- lib/utils.bash
+++ lib/utils.bash
@@ -65,7 +65,7 @@ install_version() {
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
-		rm -rf "$install_path"
+		# rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
 }
